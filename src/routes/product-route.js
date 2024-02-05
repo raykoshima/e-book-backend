5a8d5bfca9f5f91baa3111957786048ecb3a0e7c
raykoshima <truemoneythailand@gmail.com>
2024-02-05 15:14:11 +0700

    add authenticate to product

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const productController = require("../controllers/product-controller");
+const authenticate = require('../middlewares/authorization')
 
 const router = express.Router();
 
-router.get("/",productController.getallProduct)
+router.get("/",authenticate,productController.getallProduct)
+
 router.get("/page/:page",productController.getProductByPage)
 router.get("/id/:id",productController.getProductByID)
 router.get("/search",productController.searchProduct)
